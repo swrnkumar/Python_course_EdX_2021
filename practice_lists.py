82cedbb078505a9e1fd6ba16a684151365602958
swrnkumar <swrnkumar@yahoo.com>
2020-12-30 18:54:51 +0100

    practice lessons EdX course

---
+++ practice_lists.py
@@ -0,0 +1,35 @@
+#practice list operations
+
+L = [2,76,98894]
+
+print(L)
+
+L.append(546)
+print(L)
+
+L1 = [4,5,8,98]
+L2 = [45,6,9282]
+
+L3 = L1 + L2
+print(L3)
+
+L1.extend(["a","b","hello world"])
+
+print(L1)
+L1.remove(5)
+print(L1)
+L1.pop()
+print(L1)
+
+s = "Hello"
+
+Alpha = list(s)
+print(Alpha)
+
+L2.sort()
+print(L2)
+
+listA = [100,0,1,4,7]
+listA.extend([4, 1, 6, 3, 4])
+
+print(listA)
