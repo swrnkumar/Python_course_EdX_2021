aaa64cd07d6bab219f5b291dc06b0d0e4c4b92b8
swrnkumar <swrnkumar@yahoo.com>
2020-12-31 22:47:41 +0100

    practice lessons

---
+++ practice_dicts.py
@@ -0,0 +1,11 @@
+# Practice exercises from EdX course 6.00.1.x
+
+grades = {'Anna':'B', 'John':'A', 'Matt':'C', 'Dave':'C'}
+print(grades['Anna'])
+
+grades['Harry'] = 'D'
+
+print(grades)
+
+print(grades.keys())
+print(grades.values())  
