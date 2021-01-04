185f945d48cdd3dc32ecca00946b857dc6af8194
swrnkumar <swrnkumar@yahoo.com>
2021-01-04 19:22:08 +0100

    hangman game updates

--- PS3_hangman.py
+++ PS3_hangman.py
@@ -59,28 +59,28 @@ def isWordGuessed(secretWord, lettersGuessed):
         else:
             return False
 
-WordofChoice = input("Enter your secret word: ")
+# WordofChoice = input("Enter your secret word: ")
 
-GuessList = input("Enter list of letters guessed: ")
+# GuessList = input("Enter list of letters guessed: ")
 
-GameResult = isWordGuessed(WordofChoice, GuessList)
+# GameResult = isWordGuessed(WordofChoice, GuessList)
 
-print(f"Game results {GameResult}")
+# print(f"Game results {GameResult}")
 
 
-# def getGuessedWord(secretWord, lettersGuessed):
-#     '''
-#     secretWord: string, the word the user is guessing
-#     lettersGuessed: list, what letters have been guessed so far
-#     returns: string, comprised of letters and underscores that represents
-#       what letters in secretWord have been guessed so far.
-#     '''
-#     for s in secretWord:
-#         if s in lettersGuessed:
-#             guessedString = guessedString + s
-#         else:
-#             guessedString = guessedString + underscore
-#     return guessedString
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    for s in secretWord:
+        if s in lettersGuessed:
+            guessedString = guessedString + s
+        else:
+            guessedString = guessedString + underscore
+    return guessedString
 
 
 
