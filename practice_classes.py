e216bde1f6875518e88625a50c523c454c226e90
swrnkumar <swrnkumar@yahoo.com>
2021-01-06 15:39:37 +0100

    practice sessions classes

--- practice_classes.py
+++ practice_classes.py
@@ -36,3 +36,19 @@ print(c.distance, type(c.distance))
 foo = c - origin
 
 print(foo)
+
+
+class Clock(object):
+    def __init__(self, time):
+        self.time = time
+    def print_time(self):
+        time = "6.30"
+        print(self.time)
+
+clock = Clock('5.30')
+# clock.print_time("10.30")
+
+boston_clock = Clock('5:30')
+paris_clock = boston_clock
+paris_clock.time = '10:30'
+boston_clock.print_time()
