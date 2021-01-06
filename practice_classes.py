24cd363f36c68183fc84356a5ebb55307c4e8bd4
swrnkumar <swrnkumar@yahoo.com>
2021-01-06 15:28:03 +0100

    practice lessons with classes, init functions etc

--- practice_classes.py
+++ practice_classes.py
@@ -8,13 +8,31 @@ class Coordinate(object):
         x_diff_sq = (self.x - other.x)**2
         y_diff_sq = (self.y - other.y)**2
         return (x_diff_sq + y_diff_sq)**0.5
+    def __str__(self):
+        return "<" + str(self.x) + "," + str(self.y) + ">"
+    def __sub__(self, other):
+        return Coordinate(self.x - other.x, self.y - other.y)
 
 
 c = Coordinate(4,3)
 origin = Coordinate(0,0)
 
+print (c, origin)
+
 print(c.distance(origin))
 
 result_coordinates = Coordinate.distance(c,origin)
 
-print(f"The distance between {c} and the origin is {result_coordinates}")
+print(f"The distance between {c} and the origin {origin} is {result_coordinates}")
+
+print(c, type(c))
+print(Coordinate, type(Coordinate))
+
+print(isinstance(c, Coordinate))
+print(isinstance(origin, Coordinate))
+print(isinstance(c.distance, Coordinate))
+print(c.distance, type(c.distance))
+
+foo = c - origin
+
+print(foo)
