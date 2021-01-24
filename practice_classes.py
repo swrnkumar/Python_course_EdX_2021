c71d0fcbed3c8113cb811e90a183c1bd2235a568
swrnkumar <swrnkumar@yahoo.com>
2021-01-24 20:41:25 +0100

    practice lessons classes - Edx course

--- practice_classes.py
+++ practice_classes.py
@@ -54,3 +54,15 @@ paris_clock.time = '10:30'
 boston_clock.print_time()
 
 
+class fraction(object):
+    def __init__(self, numer, denom):
+        self.numer = numer
+        self.denom = denom
+    def __str__(self):
+        return str(self.numer) + '/' + str(self.denom)
+
+oneHalf = fraction(1,2)
+twoThirds = fraction(2,3)
+
+print(oneHalf)
+print(twoThirds)
