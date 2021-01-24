d50eb88449e2e4928db3d60b2352918f0b01ce04
swrnkumar <swrnkumar@yahoo.com>
2021-01-24 21:32:55 +0100

    classes practice EdX course

--- practice_classes.py
+++ practice_classes.py
@@ -60,9 +60,48 @@ class fraction(object):
         self.denom = denom
     def __str__(self):
         return str(self.numer) + '/' + str(self.denom)
+    def getNumer(self):
+        return self.numer
+    def getDenom(self):
+        return self.denom
 
 oneHalf = fraction(1,2)
 twoThirds = fraction(2,3)
 
 print(oneHalf)
 print(twoThirds)
+
+print(oneHalf.getNumer())
+print(twoThirds.getDenom())
+
+class ingSet(object):
+    def __init__(self):
+        self.vals = []
+    def insert(self, e):
+        if not e in self.vals:
+            self.vals.append(e)
+    def member(self, e):
+        return e in self.vals
+    def remove(self, e):
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found in set')
+    def __str__(self):
+        self.vals.sort()
+        result = ''
+        for e in self.vals:
+            result = result + str(e) + ','
+        return '{' + result[:-1] + '}'
+
+s = ingSet()
+print (s)
+s.insert(23)
+s.insert(90)
+s.insert(8)
+s.insert(23)
+s.insert(764)
+print(s)
+print(s.member(765))
+print(s.member(8))
+s.remove(4)
